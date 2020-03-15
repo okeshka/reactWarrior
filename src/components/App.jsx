47a1db489fb48b9cb921dadec912f94382086906
Oleg Zakladnyi <42720890+okeshka@users.noreply.github.com>
2020-03-15 17:19:27 +0200

    populatity vs popularity
    !!!!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
         this.state = {
             movies: [],
             moviesWilllwatch: [],
-            sort_by: "populatity.desc",
+            sort_by: "popularity.desc",
         };
      //this.removeMovie = this.removeMovie.bind(this);
     }
